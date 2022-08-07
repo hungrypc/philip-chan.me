acc62564e4b5626d3adddd25ea6ec9e69a1d3d75
hpc <philipchanhk626@gmail.com>
2022-08-07 18:55:09 -0400

    fuck man

--- components/ItsaMe.tsx
+++ components/ItsaMe.tsx
@@ -6,6 +6,6 @@ const image = images[0]
 
 export const ItsaMe: React.FC = () => (
   <div key={image.src} className='max-w-xl mx-auto'>
-    <Image src={image} alt='picture of me' placeholder='blur' objectFit='contain' height={522} width={576} priority />
+    <Image src={image} alt='picture of me' placeholder='blur' objectFit='contain' priority />
   </div>
 )
