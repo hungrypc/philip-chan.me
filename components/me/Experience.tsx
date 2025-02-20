337987d4e0acda106333e2ca67edb5f4d5521b5d
Phil Chan <35395991+hungrypc@users.noreply.github.com>
2025-02-20 16:41:02 -0500

    update exp

--- components/me/Experience.tsx
+++ components/me/Experience.tsx
@@ -2,18 +2,18 @@ import { parseDate } from '@utils/date'
 
 const experience = [
   {
-    company: 'Guiker',
-    position: 'Full-stack Software Developer',
-    startDate: 'Jul/2020',
-    endDate: 'Nov/2022',
+    company: 'Shippie',
+    position: 'Sr. Full-stack Software Engineer',
+    startDate: 'Nov/2024',
+    endDate: 'Present',
     details: (
       <p>
-        Guiker is a software platform on which tenants and landlords are able to connect, sign relevant documents, and
-        pay/receive rent throughout the lease term. Responsibilities include project ownership, implementation and
-        maintanence of various (micro) services, building out product interface, and assisting in product-ops support.
+        Worked on functionalities for the platform that enabled users to manage their shipping and logistics needs.
+        Features include third-party e-commerce API integrations, re-building billing system, bulk import packages via
+        label scanner with text-recognition. Overseeing and conducting migration of legacy system to new architecture.
       </p>
     ),
-    stack: 'React, TypeScript, AWS, MongoDB, MySQL',
+    stack: 'Typescript, Nodejs, React, GCloud, Firebase, PostgreSQL',
   },
   {
     company: 'Yoop',
@@ -22,12 +22,28 @@ const experience = [
     endDate: 'Present',
     details: (
       <p>
-        Yoop is a platform that offers the event experience with ticketing solutions for fans through video attendance
-        and e-solution, replacing tickets with tokens, releasing a robust suite creator tools, streamlining event
-        inventory distribution, and building the first trusted and transparent fan journey.
+        Designed architecture and implemented system migration for multi-region data management, compliant with GDPR to
+        enable expansion into Europe. Improved infrastructure tools to automate and deploy services, resulting in
+        quicker disaster recovery response time, as well as simplified management of deploying services across regions.
+        Built automated system to scale resources based on custom metrics designed within the context of ticketing,
+        making resource management efficient and reducing costs.
       </p>
     ),
-    stack: 'Spring, Java, AWS, Redis, MySQL',
+    stack: 'Java, Spring, Terraform, AWS, Redis, MySQL',
+  },
+  {
+    company: 'Guiker',
+    position: 'Full-stack Software Developer',
+    startDate: 'Jul/2020',
+    endDate: 'Nov/2022',
+    details: (
+      <p>
+        Worked on the consumer app that enabled tenants and landlords to connect. Built and managed features for chat
+        platform, rental listing management, booking system, document signing, and payment transaction management.
+        Contributed to internal tools for building and deploying services.
+      </p>
+    ),
+    stack: 'TypeScript, React, AWS, MongoDB, MySQL',
   },
 ]
 
@@ -39,9 +55,9 @@ export const Experience: React.FC = () => (
         <li key={company} className='mb-4 border-b border-stone-200 pb-2 pl-0 before:contents dark:border-stone-900'>
           <section className='flex flex-col justify-between gap-1 sm:flex-row sm:gap-6'>
             <div className='flex gap-1 text-center text-sm text-stone-600 dark:text-stone-400 sm:flex-col sm:gap-0 sm:pl-1 sm:pt-1'>
-              <time dateTime={parsedStartDate.tag}>{parsedStartDate.short}</time>
-              <div className='sm:leading-4'>{'-'}</div>
               <time dateTime={parsedEndDate.tag}>{parsedEndDate.short}</time>
+              <div className='sm:leading-4'>{'-'}</div>
+              <time dateTime={parsedStartDate.tag}>{parsedStartDate.short}</time>
             </div>
             <div>
               <h3 className='m-0 mb-1 text-base font-semibold'>{position}</h3>
